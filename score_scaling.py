16b1f8c8e448a824a6e672a7071b0a95e29a1dcc
tcha7 <133793300+tcha7@users.noreply.github.com>
2023-11-09 11:16:38 +0700

    Add type hint

--- score_scaling.py
+++ score_scaling.py
@@ -1,6 +1,23 @@
 import numpy as np
+import pandas as pd
+from typing import Union
 
-def scale_score(score, pdo = 60, base_odd = 1, base_score = 600):
-    alpha = pdo/np.log(2)
-    scaled_score = base_score - alpha*np.log(base_odd) + alpha*np.log((1-score)/score)
+
+def scale_score(
+    score: Union[np.ndarray, pd.Series, pd.DataFrame],
+    pdo: float = 60,
+    base_odd: float = 1,
+    base_score: float = 600,
+) -> Union[np.ndarray, pd.Series, pd.DataFrame]:
+    """
+    odds = prob_good/prob_bad
+    PDO = points-to-double-odds = the number of points before the odd doubles
+    base_odd and base_score are the pre-defined point. For example, if we want to define
+    the score 600 to have odd 1, then we set base_odd = 1, and base_score = 600
+    """
+
+    alpha = pdo / np.log(2)
+    scaled_score = (
+        base_score - alpha * np.log(base_odd) + alpha * np.log((1 - score) / score)
+    )
     return scaled_score
